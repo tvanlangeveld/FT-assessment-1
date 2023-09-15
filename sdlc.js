13fc50b3ffb1bab03bc5054609f5d434cb76ceda
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-15 08:15:09 -0700

    First commit with completed SDLC file.

---
+++ sdlc.js
@@ -0,0 +1,45 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+1. 'Planning'
+2. 'Analysis of Requirements'
+3. 'Design'
+4. 'Implementation'
+5. 'Testing'
+6. 'Maintain'
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+
+ const AnalysisOfRequirement = "This is The process that is used to determine the needs of a product, what does this need in order to have the correct functions for the user and the functions for the business as well."
+
+
+ const Design = "This part of the process is to transform all the toughts and idea into suitable form for the users, this also give a blue print for the programmers to start coding the software needed for the desired features of the software."
+
+ const Implementation = "This is pretty straight forward. This step is where the programming actually happens and software is actually created."
+
+ const testing = "During this stage we are testing the software that has been built to make sure it is working properly based on our design and analysis of requirements and look for any bugs in our software. "
+
+ const maintain = "After we have deployed our code. During this phase we maintain the current code base and continue to improve what is already there."
+
+//  of course we can do this cycle as many times as needed to create the software we are desiring.
